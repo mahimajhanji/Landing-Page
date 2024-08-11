15d0ff27354eed7716257aaf9e0d54003a221ee3
Mahima Jhanji <mahimajhanji@gmail.com>
2024-08-11 16:47:20 +0530

    added file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const navbar = document.getElementById('navbar');
+
+
+function handleScroll() {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+}
+
+
+window.addEventListener('scroll', handleScroll);
